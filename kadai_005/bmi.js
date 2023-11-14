c838813d002076ac9dfb58b1d4e795128c7c8ef8
yumimts.aile.0407.0318@gmail.com <yumimts.aile.0407.0318@gmail.com>
2023-11-14 14:44:19 +0900

    first commit

---
+++ kadai_005/bmi.js
@@ -0,0 +1,15 @@
+//体重と身長のBMI（体格指数）を出力
+
+// 変数の宣言
+let taijyuu;
+let sincyou;
+
+// 値の代入
+taijyuu = 68;
+sincyou = 1.7;
+
+ // 計算式
+ bmi= taijyuu/(sincyou*sincyou);
+
+// コンソールへの出力
+console.log(bmi);
