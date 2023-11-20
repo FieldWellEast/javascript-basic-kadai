cf5c6465613248a8aa5cb575002f89783f7358eb
yumimts.aile.0407.0318@gmail.com <yumimts.aile.0407.0318@gmail.com>
2023-11-20 21:14:55 +0900

    最初のコミット

---
+++ kadai_021/event.js
@@ -0,0 +1,28 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const textValue = document.getElementById('text');
+
+
+// HTML要素がクリックされたときにイベント処理を実行する
+addBtn.addEventListener('click', () => {
+
+setTimeout(() => {
+    // ドキュメントが読み込まれたときに関数を実行する
+    textValue.textContent ='ボタンをクリックしました';
+    }, 2000);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
